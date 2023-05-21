64bba0266ec19763339c86f55ff8101402199a61
ShreyaSingh13 <112298801+ShreyaSingh13@users.noreply.github.com>
2023-05-22 01:15:08 +0530

    Add files via upload

---
+++ Line_Encoder_Scrambler.cpp
@@ -0,0 +1,10 @@
+
+/*INPUT:- Ask user for digital data stream generation i.e., whether
+user wants completely random sequence or a random sequence with
+fixed sub-sequences (usually used for scrambling) and the type of
+encoding. For encoding you need to provide user with various
+options (NRZ-L, NRZ-I, Manchester, Differential Manchester, AMI). If
+user asks for AMI, you need to pop a query whether scrambling is
+needed or not, if answer is yes next query would ask about the type
+of scrambling.
+CODE:-*/
